9de91f3568a814413c81fd9a89a6819f4fedbad3
nstprkp <113131930+nstprkp@users.noreply.github.com>
2023-02-19 00:10:50 +0300

    Delete header.h

--- header.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-struct flat;
-int input_check();
-void input_struct(struct flat *skyscraper, int n);
-void typeSort(struct flat *skyscraper, int n);
-void output(struct flat *skyscraper, int n);
-void sortBuble(struct flat *skyscraper, int t, int n);
